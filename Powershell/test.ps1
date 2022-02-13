03be428d729d05d83e6133a015f5e0cf7a39cd71
John Snow <66561475+PowerOps-MK@users.noreply.github.com>
2022-02-13 16:21:56 +0100

    Update test.ps1

--- Powershell/test.ps1
+++ Powershell/test.ps1
@@ -1,1 +1,8 @@
+#Install modules
+Install-Module az -Scope AllUsers -Force
+Install-Module az.resourcegraph -Scope AllUsers -Force
+Install-Module aztable -Scope AllUsers -Force
+Install-Module microsoft.graph -Scope AllUsers -Force
+Install-Module azureadpreview -Scope AllUsers -Force
+
 Write-Host "Hello World"
