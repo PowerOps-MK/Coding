62073d652e4ca2a8ae009431b4781c94a6b0e5f4
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-15 21:45:28 +0100

    Update main.go

--- Docker/go/main.go
+++ Docker/go/main.go
@@ -1,38 +1,14 @@
-// Sample run-helloworld is a minimal Cloud Run service.
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "fmt"
+    "net/http"
 )
 
 func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    })
 
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-        if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+    http.ListenAndServe(":8080", nil)
 }
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
-}
-
-// [END run_helloworld_service]
-// [END cloudrun_helloworld_service]
