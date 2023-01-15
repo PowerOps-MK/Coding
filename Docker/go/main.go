e0fbdc522ab521983f0bf3619be01c0d1e0c7a62
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-15 22:48:48 +0100

    Update main.go

--- Docker/go/main.go
+++ Docker/go/main.go
@@ -9,3 +9,7 @@ func main() {
     http.HandleFunc("/", HelloServer)
     http.ListenAndServe(":8080", nil)
 }
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
