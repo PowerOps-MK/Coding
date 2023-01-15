d3ee4b7e36da7cbdba93c36f25fbf8c93eaf1962
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-15 23:15:58 +0100

    Update main.go

--- Docker/go/main.go
+++ Docker/go/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
       //  fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+        fmt.Println("hello.")
     })
 
     server := &http.Server{
