d3dc520dea7186617255fc8bb9e693d3c3dd4841
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 13:51:16 +0100

    Update __main__.py

--- Python/xata/__main__.py
+++ Python/xata/__main__.py
@@ -6,8 +6,8 @@ fake = Faker()
 
 # Creata Xata client
 client = XataClient(
-   api_key="xau_nc9NKeXvgvHuS1AoR5NOwIyWMuPc2XUc7", 
-   db_url="https://sifon-lade-0z-s-workspace-h9pso8.eu-west-1.xata.sh/db/powerops-db", 
+   api_key="xau_nc9NKeXvgvHuS1AoR5NOwIyWMuPc2XUc7",
+   db_url="https://sifon-lade-0z-s-workspace-h9pso8.eu-west-1.xata.sh/db/powerops-db",
    branch_name="main"
 )
 
