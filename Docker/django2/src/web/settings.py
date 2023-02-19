80ce20e44f6224ed5ac348f3ae53bb3ae6770a2a
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-19 10:39:34 +0100

    Update settings.py

--- Docker/django2/src/web/settings.py
+++ Docker/django2/src/web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret
-SECRET_KEY = "django-insecure-i@)y0okk6ycdh(ignziua=dbpi47#seqwk5hny(j$v^yh-o24m"
+SECRET_KEY = "foo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -123,4 +123,4 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-ALLOWED_HOSTS = ["django2-mktest.herokuapp.com"]
+ALLOWED_HOSTS = [""]
